b6126250a1382b158669f2dff22573e535fca83b
Christa Kelly <46685557+christadkelly@users.noreply.github.com>
2023-08-03 19:49:27 +0000

    answers for exercises 41-60

--- exercises/049-computeAverageOfNumbers/app.js
+++ exercises/049-computeAverageOfNumbers/app.js
@@ -1,1 +1,15 @@
 // Write your function here
+function computeAverageOfNumbers(nums) {
+    if (nums.length == 0) return 0;
+
+    let total = 0;
+    for (let i = 0; i < nums.length; i++) {
+      total += nums[i];
+    }
+    let avg = total / nums.length;
+    return avg;
+}
+
+let input = [1, 2, 3, 4, 5];
+let output = computeAverageOfNumbers(input);
+console.log(output); 
--- exercises/056-getElementsAfter/app.js
+++ exercises/056-getElementsAfter/app.js
@@ -1,6 +1,8 @@
 function getElementsAfter(array, n) {
   // your code here
-  
+  let length = array.length;
+  let after = n+1;
+  return array.slice(after, length);
 }
 
 let output = getElementsAfter(['a', 'b', 'c', 'd', 'e'], 2); 
